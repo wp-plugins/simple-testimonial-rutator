c5b3ad2bcf96d5004148f1292412db2d5304b3d4
india-web-developer <india-web-developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-18 06:20:15 +0000

    update version related message
    git-svn-id: https://plugins.svn.wordpress.org/simple-testimonial-rutator/tags/1.1@934196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple-testimonial-rotator.php
+++ simple-testimonial-rotator.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Testimonial Rotator
 Plugin URI: http://raghunathgurjar.wordpress.com
 Description: "Simple Testimonial Rotator" is a very simple plugins for add to testimonials on your site. 
-Version: 1.0
+Version: 1.1
 Text Domain: raghunath
 Author: Raghunath Gurjar
 Author URI: http://www.facebook.com/raghunathprasadgurjar
